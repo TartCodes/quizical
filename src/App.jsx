d15118740498edb7a5d1fad915c433952a2a5d6c
TartCodes <tartcodes@gmail.com>
2023-01-05 10:32:17 -0500

    working on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Start from "./Components/Start";
 
 function App() {
+	//states
 	const [start, setStart] = useState(false);
+	const [quizData, setQuizData] = useState([]);
+	const [quizOptions, setQuizOptions] = useState({
+		category: "",
+		difficulty: ""
+	});
+	//api
+	useEffect(
+		() => {
+			fetch(
+				`https://opentdb.com/api.php?amount=5&category=${quizOptions.category}&difficulty=${quizOptions.difficulty}`
+			)
+				.then((res) => res.json())
+				.then((data) => setQuizData(data.results));
+		},
+		[quizOptions],
+		[start]
+	);
+	console.log(quizData.map((e) => e.category));
 	//start app
 	const startQuiz = () => {
 		setStart(true);
 	};
-	return <main>{start ? "" : <Start start={startQuiz} />}</main>;
+
+	//update quiz options
+	const updateQuizOptions = (e) => {
+		const { name, value } = e.target;
+		setQuizOptions((prevOptions) => {
+			return {
+				...prevOptions,
+				[name]: value
+			};
+		});
+	};
+
+	//render categories?
+	// const renderCatergories = () => {};
+
+	return (
+		<main>
+			{start ? (
+				""
+			) : (
+				<Start
+					start={startQuiz}
+					updateQuizOptions={updateQuizOptions}
+				/>
+			)}
+		</main>
+	);
 }
 
 export default App;
--- src/Components/Start.jsx
+++ src/Components/Start.jsx
@@ -3,6 +3,27 @@ const Start = (props) => {
 		<div>
 			<h1>Quizzical!</h1>
 			<p>A short and simple trivia game</p>
+			<div className="quiz-options">
+				<label htmlFor="difficulty">Difficulty</label>
+				<select
+					name="difficulty"
+					onChange={props.updateQuizOptions}
+				>
+					<option value="">Any Difficulty</option>
+					<option value="easy">Easy</option>
+					<option value="medium">Medium</option>
+					<option value="hard">Hard</option>
+				</select>
+				<label htmlFor="category">Select Category</label>
+				<select
+					id="category"
+					name="category"
+					onChange={props.updateQuizOptions}
+				>
+					<option value="1">Any Category</option>
+					<option value="2">Entertainment</option>
+				</select>
+			</div>
 			<button>Begin</button>
 		</div>
 	);
